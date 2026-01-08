25fe1586789dccf8d0030f1250cef34e45288770
TinPChome <nlttin@gmail.com>
2026-01-08 14:32:08 +0700

    fix some config for free subcripttion

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,17 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   subscription_id = var.subscription_id
 }
 
