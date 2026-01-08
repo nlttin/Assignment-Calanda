1174a412a052a3cffdf8d4d90081e8c0a287c19d
TinPChome <nlttin@gmail.com>
2026-01-08 15:50:32 +0700

    change asociate nic for access

--- terraform/main.tf
+++ terraform/main.tf
@@ -91,9 +91,10 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-# Attach NSG to Monitoring Server (VM0)
+# Attach NSG to Monitoring Server (2VMs)
 resource "azurerm_network_interface_security_group_association" "nsg_assoc" {
-  network_interface_id      = azurerm_network_interface.nic[0].id
+  count                     = 2
+  network_interface_id      = azurerm_network_interface.nic[count.index].id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 
